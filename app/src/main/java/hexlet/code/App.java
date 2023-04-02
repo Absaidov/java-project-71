c87e57d0a556a5c2279332d0869c01661cef9f3c
JamaL <jamalikowow@mail.ru>
2023-04-02 20:40:49 +0300

    export from macbook

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,6 +1,22 @@
 package hexlet.code;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.io.File;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.util.concurrent.Callable;
+
 public class App {
+    @Option(names = "-v", description = "Usage: gendiff [-hV]\n" +
+            "Compares two configuration files and shows a difference.\n" +
+            "  -h, --help      Show this help message and exit.\n" +
+            "  -V, --version   Print version information and exit.")
+//    boolean create;
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
