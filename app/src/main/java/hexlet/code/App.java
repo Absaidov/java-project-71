aa114a61e9e060453d0a1349c0712240e758de39
Absaidov <dabsaidov@bk.ru>
2023-04-03 15:52:27 +0300

    third step of project

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,19 +5,32 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
-import java.io.File;
-import java.math.BigInteger;
-import java.nio.file.Files;
-import java.security.MessageDigest;
-import java.util.concurrent.Callable;
-
-public class App {
-    @Option(names = "-v", description = "Usage: gendiff [-hV]\n" +
-            "Compares two configuration files and shows a difference.\n" +
-            "  -h, --help      Show this help message and exit.\n" +
-            "  -V, --version   Print version information and exit.")
+//public class App {
+//    @Option(names = "-h", description = "Usage: gendiff [-hV]\n" +
+//            "Compares two configuration files and shows a difference.\n" +
+//            "  -h, --help      Show this help message and exit.\n" +
+//            "  -V, --version   Print version information and exit.")
 //    boolean create;
+@Command(name = "gendiff", version = "gendiff 1.0", mixinStandardHelpOptions = true, description = "Compares two configuration files and shows a difference.")
+public class App implements Runnable {
+    @Override
+    public void run() {
+        // The business logic of the command goes here...
+        // In this case, code for generation of ASCII art graphics
+        // (omitted for the sake of brevity).
+        helloWorld();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+    public static String helloWorld(){
+        return "Hello World!";
     }
 }
+
+//    public static void main(String[] args) {
+//        System.out.println("Hello World!");
+//    }
+//}
