e95f96366f4778570a5c63b6cdcade51ce0cfc03
Absaidov <dabsaidov@bk.ru>
2023-04-11 17:16:32 +0300

    half of step 5

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -6,17 +6,7 @@ import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
 import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
-//import static hexlet.code.Differ.generate;
-
-//public class App {
-//    @Option(names = "-h", description = "Usage: gendiff [-hV]\n" +
-//            "Compares two configuration files and shows a difference.\n" +
-//            "  -h, --help      Show this help message and exit.\n" +
-//            "  -V, --version   Print version information and exit.")
-//    boolean create;
 @Command(name = "gendiff", version = "gendiff 1.0", mixinStandardHelpOptions = true, description = "Compares two configuration files and shows a difference.")
 public class App implements Runnable {
     @Parameters(index = "0", description = "path to first file.")
@@ -27,36 +17,15 @@ public class App implements Runnable {
     String format;
     @Override
     public void run() {
-        // The business logic of the command goes here...
-        // In this case, code for generation of ASCII art graphics
-        // (omitted for the sake of brevity).
-//        helloWorld();
-//        return "Hello World!";
-        System.out.println("Hello World!");
-//        System.out.println();
-//        Differ.main();
-//        try {
         try {
-            Differ.generate(filepath1, filepath2);
+            System.out.println(Differ.generate(filepath1, filepath2));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
     }
 
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
-//        System.out.println();
     }
-//    public static String helloWorld(){
-//        return "Hello World!";
-//    }
 }
-
-//    public static void main(String[] args) {
-//        System.out.println("Hello World!");
-//    }
-//}
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -4,83 +4,93 @@ import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-//import java.util.HashMap;
-import java.util.HashMap;
-import java.util.Map;
 
-//import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.core.JsonParser;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.fasterxml.jackson.*;
 
 public class Differ {
-    public static String generate(File file1, File file2) throws Exception {
+    public static Map<String, Object> generate(File file1, File file2) throws Exception {
         Path path = Paths.get(file1.toURI()).toAbsolutePath().normalize();
         Path path2 = Paths.get(file2.toURI()).toAbsolutePath().normalize();
 
         if (!Files.exists(path)) {
             throw new Exception("File '" + path + "' does not exist");
         }
+
         String content = Files.readString(path);
         String content2 = Files.readString(path2);
-        Map<String, Object> mapFile3 = new HashMap<>();
         ObjectMapper objectMapper = new ObjectMapper();
-        Map<String, Object> mapFile1 = new HashMap<>();
-        mapFile1
-                = objectMapper.readValue(content, new TypeReference<Map<String, Object>>() {
-        });
+        Map<String, Object> mapFile3 = new LinkedHashMap<>();
 
-        Map<String, Object> mapFile2
-                = objectMapper.readValue(content2, new TypeReference<Map<String, Object>>() {
-        });
-//        System.out.println(content);
-//        System.out.println(content2);
-//        System.out.println();
-//        System.out.println(mapFile1);
-//        System.out.println(mapFile2);
-//        System.out.println(mapFile1.entrySet());
+        Map<String, Object> mapFile1 = objectMapper.readValue(content,
+                new TypeReference<TreeMap<String, Object>>() {
+                });
+
+        Map<String, Object> mapFile2 = objectMapper.readValue(content2,
+                new TypeReference<TreeMap<String, Object>>() {
+                });
+
         for (Map.Entry<String, Object> mapFile1s : mapFile1.entrySet()) {
-            for (Map.Entry<String, Object> mapFile2s : mapFile2.entrySet()){
-                if (mapFile1s.getKey().equals(mapFile2s.getKey()) && mapFile1s.getValue().equals(mapFile2s.getValue())){
+            for (Map.Entry<String, Object> mapFile2s : mapFile2.entrySet()) {
+                if (!mapFile1s.getKey().equals(mapFile2s.getKey()) && !mapFile1s.getValue().equals(mapFile2s.getValue())) {
+                    mapFile3.put("- " + mapFile1s.getKey(), mapFile1s.getValue());
+                    break;
+                }
+                if (mapFile1s.getKey().contains(mapFile2s.getKey()) && mapFile1s.getValue().equals(mapFile2s.getValue())) {
                     mapFile3.put(mapFile1s.getKey(), mapFile1s.getValue());
+                    break;
                 }
 
-                if(mapFile1s.getKey().equals(mapFile2s.getKey()) && !(mapFile1s.getValue().equals(mapFile2s.getValue()))){
-                    System.out.println("- " + mapFile1s.getKey() + ":" + " " +  mapFile1s.getValue());
+            }
+        }
+        for (Map.Entry<String, Object> mapFile2s : mapFile2.entrySet()) {
+            for (Map.Entry<String, Object> mapFile1s : mapFile1.entrySet()) {
+//                var name1 =
+                var mapFiles2Key = mapFile2s.getKey();
+                var mapFiles2Value = mapFile2s.getValue();
+                var mapFiles1Key = mapFile1s.getKey();
+                var mapFiles1Value = mapFile1s.getValue();
+                System.out.println(mapFiles2Key + " 2");
+                System.out.println(mapFiles1Key + " 1");
+                if (mapFiles2Key.equals(mapFiles1Key) && !mapFiles2Value.equals(mapFiles1Value)) {
+                    mapFile3.put("+ " + mapFile2s.getKey(), mapFile2s.getValue());
+//                    System.out.println(mapFiles1Key);
+                    break;
                 }
-
-//                if(mapFile1s.getKey().equals(mapFile2s.getKey()) && !(mapFile1s.getValue().equals(mapFile2s.getValue()))){
-//                    mapFile3.put(mapFile1s.getKey(), mapFile1s.getValue());
-////                    mapFile3.put(mapFile2s.getKey(), mapFile2s.getValue());
-////                    System.out.println(mapFile2s.getKey());
-////                    System.out.println(mapFile2s.getValue());
-////                    System.out.println(mapFile1s.getKey());
-////                    System.out.println(mapFile1s.getKey());
-////                    System.out.println(mapFile1s.getValue());
+                if (mapFile2s.getKey().equals(mapFile1s.getKey()) && mapFile2s.getValue().equals(mapFile1s.getValue())) {
+                    mapFile3.put(mapFile2s.getKey(), mapFile2s.getValue());
+                    break;
+                }
+//                if (!mapFiles2Key.equals(mapFiles1Key) && !mapFiles2Value.equals(mapFiles1Value)){
+//                    mapFile3.put("+ " + mapFile2s.getKey(), mapFile2s.getValue());
+////                    System.out.println(mapFiles2Key);
+////                    System.out.println(mapFiles2Value);
+////                    System.out.println(mapFiles2Key);
+//                    System.out.println(mapFiles1Key);
+//                    break;
 //                }
-//                if(mapFile1s.getKey().equals(mapFile2s.getKey()) && !(mapFile1s.getValue().equals(mapFile2s.getValue()))){
-////                    mapFile3.put(mapFile1s.getKey(), mapFile1s.getValue());
-//                    mapFile3.put(mapFile2s.getKey(), mapFile2s.getValue());
-////                    System.out.println(mapFile2s.getKey());
-////                    System.out.println(mapFile2s.getValue());
-////                    System.out.println(mapFile1s.getKey());
-////                    System.out.println(mapFile1s.getKey());
-////                    System.out.println(mapFile1s.getValue());
+
+
+//                if (mapFiles2Key.equals(mapFiles1Key) && mapFiles2Value.equals(mapFiles1Value)){
+////                    System.out.println(mapFiles1Key);
+////                    System.out.println(mapFiles1Value);
+//                    break;
+//                }
+//                if (!mapFiles2Key.equals(mapFiles1Key) && !mapFiles2Value.equals(mapFiles1Value)){
+//                    mapFile3.put("+ " + mapFile2s.getKey(), mapFile2s.getValue());
+////                    System.out.println(mapFiles2Key);
+////                    System.out.println(mapFiles2Value);
+////                    System.out.println(mapFiles2Key);
+//                    System.out.println(mapFiles1Key);
+//                    break;
 //                }
             }
-
-//            System.out.println(
-//                    "My key is = " + mapFile1s.getKey() + " my value is = " + mapFile1s.getValue()
-
-
         }
-        System.out.println(mapFile3);
-
-//    public static Map getData(String content) throws Exception {
-//        return parse(content);
-//    }
-
-        return content;
+        return mapFile3;
     }
 }
